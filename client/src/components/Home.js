4eaa926707e3a7a7e85784ff4c6c903128854a33
Jake Bills <jakebills1@gmail.com>
2019-05-23 11:26:11 -0600

    adds text to Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {Header, } from 'semantic-ui-react';
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header as='h1'>Dan Bills Guitars</Header> 
+      <div>
+        An acoustic guitar company located in Salt Lake City, Utah specializing in steel-string guitars. Also offering repairs and custom builds. 
+      </div>
+    </div>
   );
 }
 export default Home;
