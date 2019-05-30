ad128a337631cf95c2a76d722a85197e8f4d20b2
Jake Bills <jakebills1@gmail.com>
2019-05-29 19:55:14 -0600

    adds carousel to show page

--- client/src/components/Guitars.js
+++ client/src/components/Guitars.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Card, Image, } from 'semantic-ui-react';
+import { Card, Image, Header, } from 'semantic-ui-react';
 import { Link, } from 'react-router-dom';
 const Guitars = ({ guitars, }) => {
   const renderGuitars = () => { 
     return guitars.map(g => ( 
       <Link to={{pathname: `/guitars/${g.id}`, state: { name: g.name, price: g.price, description: g.description, pictures: g.pictures}, }}>
         <Card key={g.id}>
-          <div>{g.name}</div>
-          <div>{g.price}</div>
-          <div>{g.description}</div>
+          <Header as="h1">{g.name}</Header>
+          <Header as="h3">${parseFloat(g.price).toFixed(2)}</Header>
+          {/* <div>{g.description}</div> */}
           <Image src={g.pictures[0].url} massive stackable />
         </Card>
       </Link>
@@ -17,6 +17,7 @@ const Guitars = ({ guitars, }) => {
   }
   return (
     <>
+      <Header as="h1" textAlign="center">Available Guitars</Header>
       <Card.Group>
         {renderGuitars()}
       </Card.Group>
--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState,  } from 'react';
 import { Header, Image, } from 'semantic-ui-react';
 const Show = (props) => {
   const { location: { state: { pictures, name, price, description }}} = props;
-
+  const [counter, setCounter] = useState(0); 
   return (
     <>
       <Header as="h1">{name}</Header>
       <Header as="h2">${parseFloat(price).toFixed(2)}</Header>
       <p>{description}</p>
-      <Image.Group size="large">
+      {/* <Image.Group size="large">
         { pictures.map( pic => <Image src={pic.url} /> ) }
-      </Image.Group>
+      </Image.Group> */}
+      <div onClick={ () => setCounter((counter + 1) % pictures.length) }><Image src={pictures[counter].url} /></div>
     </>
   );
 }
