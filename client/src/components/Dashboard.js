cdff55d606bc4c454ce002eecb88b39cefd80166
Jake Bills <jakebills1@gmail.com>
2019-06-02 12:06:14 -0600

    adds list of available guitars on dash

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, } from 'react';
+import axios from 'axios';
+
 const Dashboard = () => {
+  const [availableGuitars, setAvailableGuitars] = useState([]);
+  useEffect( () => {
+    axios.get("/api/guitars")
+      .then( res => setAvailableGuitars(res.data))
+      .catch( res => console.log(res))
+  }, []
+  );
+  const listAvailableGuitars = () => {
+    // lists guitars available for sale, with options to delete or edit details
+    
+    return availableGuitars.map( gtr => <li>{gtr.name}</li> )
+  }
   return (
-    <div>Dash</div>
+    <>
+      <h1>Guitars listed for sale</h1>
+      <ul>
+      { listAvailableGuitars() } 
+      </ul>
+    </>
   );
 }
 export default Dashboard;
