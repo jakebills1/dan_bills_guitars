0200358bf9c8de65808eb6cc968396588d1cb807
Jake Bills <jakebills1@gmail.com>
2019-06-02 13:43:41 -0600

    token auth working

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter, } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 import { AuthProvider, } from './providers/AuthProvider'
+import { initMiddleware, } from 'devise-axios'
+initMiddleware();
 ReactDOM.render(
   <AuthProvider>
     <BrowserRouter>
