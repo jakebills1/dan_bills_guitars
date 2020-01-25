cfbbc249e06f334bc4cd2ba0b31739e023556acf
Jake Bills <jakebills1@gmail.com>
2020-01-25 16:26:32 -0700

    refactors pictures controller

--- app/controllers/api/pictures_controller.rb
+++ app/controllers/api/pictures_controller.rb
@@ -1,30 +1,37 @@
 class Api::PicturesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_file
+  before_action :set_guitar
+
   def create
-    # iterates over files sent in params ( named file0, file1, etc ), and uploads them to cloudinary, then saves them in Database
     file_counter = 0
-    file_key = "file".concat(file_counter.to_s).to_sym
-    while params[file_key] != nil
-      file = params[file_key]
-      guitar = Guitar.find(params[:guitar_id])
-      if file
-        begin
-          ext = File.extname(file.tempfile)
-          cloud_image = Cloudinary::Uploader.upload(file,
-           public_id: file.original_filename,
-           secure: true)
-          guitar.pictures.create(url: cloud_image['secure_url'], width: cloud_image['width'], height: cloud_image['height'])
-        rescue => e
-          # if file did not save to database, should delete from cloudinary
-          if cloud_image
-            Cloudinary::Uploader.destroy(cloud_image.public_id)
-          end
-          render json: { errors: e }, status: 422
+    pictures = []
+    while @file != nil
+      begin
+        cloud_image = Cloudinary::Uploader.upload(@file,
+          public_id: @file.original_filename,
+          secure: true)
+        pictures.push({ url: cloud_image['secure_url'], width: cloud_image['width'], height: cloud_image['height'] })
+      rescue => e
+        # if file did not save to database, should delete from cloudinary
+        if cloud_image
+          Cloudinary::Uploader.destroy(cloud_image.public_id)
         end
       end
-      # increments file name (file0 to file1), which will be the next params key, or will stop loop if not present
       file_counter += 1
-      file_key = "file".concat(file_counter.to_s).to_sym
+      set_file file_counter
     end
+    @guitar.pictures.create(pictures)
+    render json: { message: "Successfully added pictures" }
   end
+
+  private
+    def set_guitar
+      @guitar = Guitar.find(params[:guitar_id])
+    end
+
+    def set_file(counter = 0)
+      file_key = "file".concat(counter.to_s).to_sym
+      @file = params[file_key]
+    end
 end
