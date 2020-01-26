77bf63df1db8db3530f068d0a8ba4e64924e4541
Jake Bills <jakebills1@gmail.com>
2020-01-26 10:01:07 -0700

    adds model level validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+  validates :name, :email, :body, :subject, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: [:create, :update] }
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :guitar
+  validates :url, :width, :height, presence: true
+  validates :width, :height, numericality: { greater_than: 0 }
+  validates :url, url: true 
 end
